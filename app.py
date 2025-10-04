1a52d041e40ed8e6695fc5561e394db14961ff8c
empire-edge-network <empire.edge.network@gmail.com>
2025-10-04 15:02:04 +0530

    app.py

--- app.py
+++ app.py
@@ -44,4 +44,8 @@ def speak():
 
 if __name__ == '__main__':
     # Use this for local testing only
+
     app.run(debug=True)
+    
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
